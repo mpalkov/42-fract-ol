a06e66418154db6c9fc495aa2bf07dc4d978f48e
Martin Palkov <martin.palkov@gmail.com>
2023-06-30 12:37:32 +0200

    move and zoom work

--- mandatory/src/handle_mouse.c
+++ mandatory/src/handle_mouse.c
@@ -12,72 +12,39 @@
 
 #include "fractol.h"
 
-int	mouse_handler(int event, int x, int y, void *params)
+void	ft_zoom(int keycode, double x_part, double y_part, t_env *env)
+{
+	double	x_len;
+	double	y_len;
+
+	x_len = env->rn_max - env->rn_min;
+	y_len = env->in_max - env->in_min;
+	if (keycode == M_WHEELDN)
+	{
+		env->rn_min -= ((x_len / ZOOM_STEP) - x_len) * x_part;
+		env->rn_max += ((x_len / ZOOM_STEP) - x_len) * (1 - x_part);
+		env->in_min -= ((y_len / ZOOM_STEP) - y_len) * y_part;
+		env->in_max += ((y_len / ZOOM_STEP) - y_len) * (1 - y_part);
+	}
+	else if (keycode == M_WHEELUP)
+	{
+		env->rn_min -= ((x_len * ZOOM_STEP) - x_len) * x_part;
+		env->rn_max += ((x_len * ZOOM_STEP) - x_len) * (1 - x_part);
+		env->in_min -= ((y_len * ZOOM_STEP) - y_len) * y_part;
+		env->in_max += ((y_len * ZOOM_STEP) - y_len) * (1 - y_part);	
+	}
+	return ;
+}
+
+int	mouse_handler(int keycode, int x, int y, void *params)
 {
 	t_env	*env;
 
 	env = params;
-	env->mouse_x = x;
-	env->mouse_y = y;
-
-	double	rn = 0;
-	double	in = 0;
-
-	double x_len = env->rn_max - env->rn_min;
-	double y_len = env->in_max - env->in_min;
-
-	double x1 = (double)x / WIN_W;
-	double y1 = (double)y / WIN_W;
-
-	if (event == M_WHEELDN)
-	{
-		/*
-		// ft_zoom()
-		printf("rn_min: %f\nrn_max: %f\nin_min: %f\nin_max: %f\n---\n", env->rn_min, env->rn_max, env->in_min, env->in_max);
-		env->rn_min -= (((env->rn_max - env->rn_min) / ZOOM_STEP) - (env->rn_max - env->rn_min)) * ((double)x / WIN_W);
-		env->rn_max += (((env->rn_max - env->rn_min) / ZOOM_STEP) - (env->rn_max - env->rn_min)) * (1 - (double)x / WIN_W);
-		env->in_min -= (((env->in_max - env->in_min) / ZOOM_STEP) - (env->in_max - env->in_min)) * ((double)y / WIN_H);
-		env->in_max += (((env->in_max - env->in_min) / ZOOM_STEP) - (env->in_max - env->in_min)) * (1 - (double)x / WIN_H);
-		printf("event: %d\nrn min = %f\nrn max = %f\nin min = %f\nin max = %f\n\n", env->rn_min, env->rn_max, env->in_min, env->in_max, event);
-		printf("x = %d y = %d\nx/win_w = %f\n1-x/win_w = %f\n\n", x, y, (double)x/WIN_W, 1 - (double)x/WIN_W);
-		// mlx_hook(env.win, M_WHEELDN, 0, mouse_handler, &env) - x / WIN_W);
-		// printf("event: %d X: %d Y: %d\n\n", event, x, y);
-		*/
-		printf("rn_min: %f\nrn_max: %f\nin_min: %f\nin_max: %f\n---\n", env->rn_min, env->rn_max, env->in_min, env->in_max);
-		env->rn_min -= ((x_len / ZOOM_STEP) - x_len) * x1;
-		env->rn_max += ((x_len / ZOOM_STEP) - x_len) * (1 - x1);
-		env->in_min -= ((y_len / ZOOM_STEP) - y_len) * y1;
-		env->in_max += ((y_len / ZOOM_STEP) - y_len) * (1 - y1);
-		printf("event: %d\nrn min = %f\nrn max = %f\nin min = %f\nin max = %f\n\n", env->rn_min, env->rn_max, env->in_min, env->in_max, event);
-		printf("event: %d X: %d Y: %d\n\n", event, x, y);
 	
-	}
-	else if (event == M_WHEELUP)
-	{
-		/*
-		// ft_zoom()
-	//	printf("rn_min: %f\nrn_max: %f\nin_min: %f\nin_max: %f\n---\n", env->rn_min, env->rn_max, env->in_min, env->in_max);
-		env->rn_min += (((env->rn_max - env->rn_min) * ZOOM_STEP) - (env->rn_max - env->rn_min)) * (1 - (double)x / WIN_W);
-		env->rn_max -= (((env->rn_max - env->rn_min) * ZOOM_STEP) - (env->rn_max - env->rn_min)) * ((double)x / WIN_W);
-		env->in_min += (((env->in_max - env->in_min) * ZOOM_STEP) - (env->in_max - env->in_min)) * (1 - (double)y / WIN_H);
-		env->in_max -= (((env->in_max - env->in_min) * ZOOM_STEP) - (env->in_max - env->in_min)) * ((double)x / WIN_H);
-		//printf("event: %d\nrn min = %f\nrn max = %f\nin min = %f\nin max = %f\n\n", env->rn_min, env->rn_max, env->in_min, env->in_max, event);
-	//	printf("x = %d y = %d\nx/win_w = %f\n1-x/win_w = %f\n\n", x, y, (double)x/WIN_W, 1 - (double)x/WIN_W);
-		// mlx_hook(env.win, M_WHEELDN, 0, mouse_handler, &env) - x / WIN_W);
-		// printf("event: %d X: %d Y: %d\n\n", event, x, y);
-		*/
-		env->rn_min -= ((x_len * ZOOM_STEP) - x_len) * x1;
-		env->rn_max += ((x_len * ZOOM_STEP) - x_len) * (1 - x1);
-		env->in_min -= ((y_len * ZOOM_STEP) - y_len) * y1;
-		env->in_max += ((y_len * ZOOM_STEP) - y_len) * (1 - y1);
-		
-	
-	}
+	if (keycode == M_WHEELDN || keycode == M_WHEELUP)
+		ft_zoom(keycode, (double)x / WIN_W, (double)y / WIN_H, env);
 	ft_fractal(env);
-	//rn = MIN_RN + y * env->rn_factor;
-	//in = MIN_IN + y * env->in_factor;
-	// printf("mouse x: %d\nmouse y: %d\nevent: %d\nRN = %f\n\nIN = %f\n\n", x, y, evnt, rn, in);
-	// printf ("rn factor: %f\nin factor: %f\n\n", env->rn_factor, env->in_factor);
 	return (0);
 }
 
@@ -115,14 +82,13 @@ int	kb_handler(int keycode, void *params)
 	env = params;
 	if (keycode == KB_ESC)
 		ft_x_close(params);
-	else if (keycode == KB_UP || keycode == KB_W)
+	else if (keycode == KB_UP || keycode == KB_W || keycode == KB_DN \
+		|| keycode == KB_S || keycode == KB_LEFT || keycode == KB_A \
+		|| keycode == KB_RIGHT || keycode == KB_D)
 		ft_move(keycode, env);
-	else if (keycode == KB_DN || keycode == KB_S)
-		ft_move(keycode, env);
-	else if (keycode == KB_LEFT || keycode == KB_A)
-		ft_move(keycode, env);
-	else if (keycode == KB_RIGHT || keycode == KB_D)
-		ft_move(keycode, env);
+	
+	
+
 	ft_printf("key pressed: %d\n", keycode);
 	return (0);
 }
